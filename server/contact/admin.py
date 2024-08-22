def935ec74f1cc011f02e8c6983d01db19daf51c
Adithya Thejas <adithyathejass.1997@gmail.com>
2024-08-22 14:57:52 +0530

    model and rest frame-work

--- server/contact/admin.py
+++ server/contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'sex', 'city', 'state', 'pin', 'active')
+    list_filter = ('sex', 'state', 'active')
+    search_fields = ('name', 'email', 'city', 'state', 'pin')
+    list_editable = ('active',)
+    ordering = ('name',)
+
+admin.site.register(Contact, ContactAdmin)
+
--- server/contact/models.py
+++ server/contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    sex = models.CharField(max_length=50)
+    active = models.BooleanField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    pin = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
