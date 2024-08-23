30d13e8c823bb118c91b09e8d488fd1863db2915
Adithya Thejas <adithyathejass.1997@gmail.com>
2024-08-23 21:57:36 +0530

    Api base url edits

--- client/src/helpers/apis/contact-apis.ts
+++ client/src/helpers/apis/contact-apis.ts
@@ -1,29 +1,39 @@
 import axios from "axios";
 import { Contact } from "../types/contact-types";
 
-export const getAllContact = async() => {
-    const res = axios.get('http://localhost:8000/api/contacts/')
-    const data = (await res).data
-    return data
+const API_BASE_URL = 'http://13.232.164.107:8000/api';
+
+// Fetch all contacts
+export const getAllContact = async () => {
+    const res = axios.get(`${API_BASE_URL}/contacts/`);
+    const data = (await res).data;
+    return data;
 }
 
-export const createContact = async(item: Contact) => {
-    const res = axios.post('http://localhost:8000/api/contacts/',item)
-    const data = (await res).data
+// Create a new contact
+export const createContact = async (item: Contact) => {
+    const res = axios.post(`${API_BASE_URL}/contacts/`, item);
+    const data = (await res).data;
+    return data;
 }
 
-export const getSingleContact = async(id: string) => {
-    const res = axios.get(`http://localhost:8000/api/contacts/${id}/`)
-    const data = (await res).data
-    return data
+// Fetch a single contact by ID
+export const getSingleContact = async (id: string) => {
+    const res = axios.get(`${API_BASE_URL}/contacts/${id}/`);
+    const data = (await res).data;
+    return data;
 }
 
-export const editContact = async(item: Contact) => {
-    const res = axios.put(`http://localhost:8000/api/contacts/${item.id}/`, item)
-    const data = (await res).data
+// Edit a contact
+export const editContact = async (item: Contact) => {
+    const res = axios.put(`${API_BASE_URL}/contacts/${item.id}/`, item);
+    const data = (await res).data;
+    return data;
 }
 
-export const deleteContact = async(id: string) => {
-    const res = axios.delete(`http://localhost:8000/api/contacts/${id}/`)
-    const data = (await res).data
+// Delete a contact
+export const deleteContact = async (id: string) => {
+    const res = axios.delete(`${API_BASE_URL}/contacts/${id}/`);
+    const data = (await res).data;
+    return data;
 }
