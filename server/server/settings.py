3964b0e7dca6375bdea06f0cf7e148209bde76d7
Adithya Thejas <adithyathejass.1997@gmail.com>
2024-08-23 21:37:10 +0530

    cors update

--- server/server/settings.py
+++ server/server/settings.py
@@ -127,5 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    'http://13.232.164.107:3000',
+    'http://localhost:3000'
 ]
